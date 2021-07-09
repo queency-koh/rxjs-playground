ddf174ccd2073cf7348cd49c7b2c1d8e98fb4f68
queency-koh <queencykoh@gmail.com>
2021-07-10 07:17:12 +0800

    My First Observable

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>RxJS Playground</h1>`;
+
+import { Observable } from 'rxjs';
+
+const observer = {
+  next: value => console.log('next', value),
+  error: error => console.log('error', error),
+  complete: () => console.log('complete')
+};
+
+const observable = new Observable(subscriber => {
+  subscriber.next('Hello');
+  subscriber.next('World');
+  subscriber.complete();
+  subscriber.next('This will not be log');
+});
+
+observable.subscribe(observer);
