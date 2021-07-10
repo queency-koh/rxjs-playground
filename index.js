c6ab92065f452ea62b9c24b22ec18dfdad418da4
queency-koh <queencykoh@gmail.com>
2021-07-10 15:39:29 +0800

    from operator

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>RxJS Playground</h1>`;
 
-import { of } from 'rxjs';
+// from can turn nearly anything into an observable
+import { from } from 'rxjs';
 
 const observer = {
   next: val => console.log('next', val),
@@ -14,12 +15,36 @@ const observer = {
 };
 
 /*
- * Emits each item you provide in sequence, synchronously.
- * of literally just loops through the items and emits them,
- * there is no flattening involved. For instance, if you pass an
- * array the entire array will be emitted, not each item within
- * the array.
+ * from can turn nearly anything into an observable
+ * When from receieves an array, it loops through each item
+ * within that array, emitting them in sequence.
  */
-const source$ = of(1, 2, 3, 4, 5);
+from([1, 2, 3, 4, 5]).subscribe(console.log);
 
-source$.subscribe(observer);
+/*
+ * This works for any array like object as well, for instance,
+ * when from receieves a string (which has a length property)
+ * it will loop through emitting each character.
+ */
+from('Hello').subscribe(console.log);
+
+/*
+ * When from receieves a promise, it will call .then, emitting
+ * the response. We will see ways to make requests using an
+ * observable interface in upcoming lessons, but for now we will
+ * just use fetch.
+ */
+from(fetch('https://api.github.com/users/octocat')).subscribe(console.log);
+
+function* hello() {
+  yield 'Hello';
+  yield 'World';
+}
+
+const iterator = hello();
+
+/*
+ * When from receieves a iterator it will drop it in a do while loop,
+ * calling .next and emitting each item until there are no more items left.
+ */
+from(iterator).subscribe(console.log);
