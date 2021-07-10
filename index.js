de0bf2ce449b15ff6782e50e41b0edb62f5f7dd2
queency-koh <queencykoh@gmail.com>
2021-07-10 13:18:20 +0800

    Create observables from DOM events using fromEvent

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>RxJS Playground</h1>`;
 
-import { Observable } from 'rxjs';
+import { fromEvent } from 'rxjs';
 
 const observer = {
-  next: value => console.log('next', value),
-  error: error => console.log('error', error),
-  complete: () => console.log('complete')
+  next: val => console.log('next', val),
+  error: err => console.log('error', err),
+  complete: () => console.log('complete!')
 };
 
-const observable = new Observable(subscriber => {
-  subscriber.next('Hello');
-  subscriber.next('World');
-  subscriber.complete();
-  subscriber.next('This will not be log');
-});
+/*
+ *  Create streams from events, given target and event name.
+ */
+const source$ = fromEvent(document, 'click');
 
-observable.subscribe(observer);
+const subOne = source$.subscribe(observer);
+const subTwo = source$.subscribe(observer);
+
+setTimeout(() => {
+  console.log('unsubscribing');
+  subOne.unsubscribe();
+}, 3000);
