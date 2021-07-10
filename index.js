0786973951ee8537b8db2b0ac929de80efd7b406
queency-koh <queencykoh@gmail.com>
2021-07-10 18:21:34 +0800

    Map, Pluck and MapTo operators

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>RxJS Playground</h1>`;
+
+import { of, fromEvent } from 'rxjs';
+import { map, pluck, mapTo } from 'rxjs/operators';
+
+of(1, 2, 3, 4, 5)
+  .pipe(map(value => value * 10))
+  .subscribe(console.log);
+
+const keyup$ = fromEvent(document, 'keyup');
+const keycode$ = keyup$.pipe(map(event => event.code));
+
+const keycodeWithPluck$ = keyup$.pipe(pluck('code'));
+
+const pressed$ = keyup$.pipe(mapTo('Key pressed'));
+
+pressed$.subscribe(console.log);
